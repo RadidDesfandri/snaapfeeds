6533fc7ba433c93e560579ad840e665bef7e34b3
RadidDesafandri <radietdesfandri@gmail.com>
2025-07-07 20:19:18 +0700

    remove floating element

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,14 @@
 import Features from "@/components/homepage/Features";
 import HeroSection from "@/components/homepage/HeroSection";
 import LayoutProvider from "@/components/LayoutProvider";
-import FloatingElemet from "@/components/ui/FloatingElemet";
+// import FloatingElemet from "@/components/ui/FloatingElemet";
 
 export default function Home() {
   return (
     <LayoutProvider className="w-full pt-32">
       <HeroSection />
       <Features />
-      <FloatingElemet />
+      {/* <FloatingElemet /> */}
     </LayoutProvider>
   );
 }
